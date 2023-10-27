c2346a012dcfa625a5730f13e82991fbfb23b189
unknown <vladimirmomiroski@gmail.com>
2023-10-27 11:05:09 +0200

    color changes and also updated padding on the page

--- src/components/button/ProjectBtn.jsx
+++ src/components/button/ProjectBtn.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import styles from './ProjectBtn.module.css'
+import React, { useContext } from "react";
+import styles from "./ProjectBtn.module.css";
+import { Context } from "../../context/Context";
 
-export default function ProjectBtn({text, link }) {
+export default function ProjectBtn({ text, link }) {
+  const { main, bgLight, bgDark } = styles;
 
-    const {main } = styles;
+  const { theme } = useContext(Context);
+
+  const whichBg = theme ? bgDark : bgLight;
 
   return (
-    <a className={main} href={link} target="_blank" rel="noreferrer">{text}</a>
-  )
+    <a className={`${main} ${whichBg}`} href={link} target="_blank" rel="noreferrer">
+      {text}
+    </a>
+  );
 }
